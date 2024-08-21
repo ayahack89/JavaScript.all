0296169312b09fa91f2e622f7e461df58eecd6ac
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-08-22 00:48:55 +0530

    Update

--- Oops/Abstraction.js
+++ Oops/Abstraction.js
@@ -1,1 +1,30 @@
 /* Abstraction */
+class Library{
+
+     constructor(bookName, authorName, availableCopies){
+          this.bookName = bookName;
+          this.authorName = authorName;
+          this.availableCopies = availableCopies;
+     }
+  
+     #checkAvailability(){
+          return this.availableCopies > 0;
+     }
+
+     #processBorrowing(){
+          return this.availableCopies --;
+     }
+
+     borrowBook(){
+          if(this.#checkAvailability()){
+               this.#processBorrowing();
+               console.log(`You have successfully borrowed "${this.bookName}" by ${this.authorName}.`);
+     
+          }else{
+               console.log(`Sorry, "${this.bookName}" is currently not available.`);    
+          }
+     }
+}
+
+const book = new Library("JavaScript - Fundamentals", "John Smith", 6);
+book.borrowBook();
